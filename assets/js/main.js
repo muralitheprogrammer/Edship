ca6ef622a5490a484659a85d1e3411848ed15a08
muralitheprogrammer <dmurali2002@gmail.com>
2025-03-13 16:00:14 +0530

    Home Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -372,5 +372,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
   startAutoScroll();
 });
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.read-more-btn').forEach(function(button) {
+    button.addEventListener("click", function() {
+      let text = this.previousElementSibling;
+      text.classList.toggle("expanded");
+
+      if (text.classList.contains("expanded")) {
+        this.textContent = "Read Less";
+      } else {
+        this.textContent = "Read More";
+      }
+    });
+  });
+});
+
+// VARIABLES
+const elH = document.querySelectorAll(".timeline li > div");
+
+// START
+window.addEventListener("load", init);
+
+function init() {
+  setEqualHeights(elH);
+}
+
+// SET EQUAL HEIGHTS
+function setEqualHeights(el) {
+  let counter = 0;
+  for (let i = 0; i < el.length; i++) {
+    const singleHeight = el[i].offsetHeight;
+
+    if (counter < singleHeight) {
+      counter = singleHeight;
+    }
+  }
+
+  for (let i = 0; i < el.length; i++) {
+    el[i].style.height = `${counter}px`;
+  }
+}
 
 
